50b1e9956a42b4698d7e7d0e534936a88fd0080c
gtkatakura <gt.katakura@gmail.com>
2021-03-15 22:50:21 -0300

    wip

--- examples/relay-multiple-endpoints/scripts/updateSchemaSdl.ts
+++ examples/relay-multiple-endpoints/scripts/updateSchemaSdl.ts
@@ -2,8 +2,8 @@ import 'make-promises-safe'
 import path from 'path'
 import fs from 'fs/promises'
 
-import { buildClientSchema, printSchema } from 'graphql'
-import { mergeSchemas } from '@graphql-tools/merge'
+import { buildClientSchema, buildSchema, print, printSchema } from 'graphql'
+import { mergeSchemas, mergeTypeDefs } from '@graphql-tools/merge'
 import chalk from 'chalk'
 
 const main = async () => {
@@ -16,34 +16,44 @@ const main = async () => {
 
   const schemas = await Promise.all(
     schemasPath.map(async schemaPath => {
-      const schemaString = await fs.readFile(
-        path.resolve(downloadedDir, schemaPath),
-        { encoding: 'utf-8' }
-      )
+      // const schemaString = await fs.readFile(
+      //   path.resolve(downloadedDir, schemaPath),
+      //   { encoding: 'utf-8' }
+      // )
 
-      const schema = buildClientSchema(JSON.parse(schemaString))
+      // const schema = buildClientSchema(JSON.parse(schemaString))
 
       const destinationFile = path.resolve(
         generatedDir,
         `${path.basename(schemaPath, '.json')}.graphql`
       )
 
-      const typeDefs = printSchema(schema)
+      const typeDefs = await fs.readFile(destinationFile, {
+        encoding: 'ascii'
+      })
 
-      await fs.writeFile(destinationFile, typeDefs)
+      console.log(typeDefs)
 
-      console.info(
-        `  ${chalk.green('✔')} Generated graphql sdl to ${path.relative(
-          rootDir,
-          destinationFile
-        )}`
-      )
+      return buildSchema(typeDefs)
 
-      return schema
+      // return buildClientSchema(typeDefs)
+
+      // const typeDefs = printSchema(schema)
+
+      // await fs.writeFile(destinationFile, typeDefs)
+
+      // console.info(
+      //   `  ${chalk.green('✔')} Generated graphql sdl to ${path.relative(
+      //     rootDir,
+      //     destinationFile
+      //   )}`
+      // )
+
+      // return schema
     })
   )
 
-  const schema = mergeSchemas({ schemas, reverseDirectives: true })
+  const schema = mergeSchemas({ schemas })
 
   const destinationFile = path.resolve(generatedDir, `schema.graphql`)
 
